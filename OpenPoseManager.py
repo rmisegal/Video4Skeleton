6572b9410c915e170f5c3f7ccb5b03e24b2cb2c9
Yoram Segal <yoramse@post.bgu.ac.il>
2022-01-26 16:41:54 +0200

    Version 0: 1 - The code to display video in the GUI window, including the option to close the program 2 - A button for sampling an image that performs both skeletal extraction and a distance matrix calculation 3- a button that only performs skeletal extraction as well as calculates the distance matrix

---
+++ OpenPoseManager.py
@@ -0,0 +1,161 @@
+import numpy as np
+import pandas as pd
+from scipy.spatial.distance import pdist, squareform
+import matplotlib.pyplot as plt
+import json
+import cv2
+import os
+import GlobalVars as glb
+#from IPython.display import Image
+#from IPython.display import display
+
+#import a function from another .ipynb nootbook file
+#from ipynb.fs.full.RmiOpenPoseUtils import tstFunc
+import logging
+import time
+
+
+
+
+class OpenPoseClass:
+
+    def __init__(self, new_folder=None):
+        self.SNAPSHOT_TMP_NAME = "tmp1"
+        self.ROOT_DIR = "C:/24D/openpose"
+        self.OP_PROGRAM_NAME = self.ROOT_DIR + "/bin/OpenPoseDemo.exe"
+        self.DATA_FOLDER = "C:/24D/RmifSample"
+        self.INPUT_SNAP_LOG_FOLDER = "/Snapshot-Log"
+        self.INPUT_SNAP_IMAGE_FOLDER = "/Snapshot-Image"
+        self.OUTPUT_FOLDER_NAME = self.DATA_FOLDER + "/Results"
+        self.CAMERA_ID = 0
+        self.update_data_folders(new_folder)
+
+    def update_data_folders(self, new_folder=None):
+        if new_folder is not None:
+            if not os.path.exists(new_folder):
+                os.mkdir(new_folder)
+                if os.path.exists(new_folder):
+                    self.DATA_FOLDER = new_folder
+                    glb.logger.debug(f'New Data folder was created: {new_folder}')
+            else:
+                self.DATA_FOLDER = new_folder
+                glb.logger.debug(f'Update to existing Data folder: {new_folder}')
+
+        self.OUTPUT_FOLDER_NAME = self.DATA_FOLDER + "/Results"
+
+        # Snap shot LOG images
+        self.FULL_IN_LOG_IMG_FOLDER = self.DATA_FOLDER + self.INPUT_SNAP_LOG_FOLDER
+        glb.logger.debug(self.FULL_IN_LOG_IMG_FOLDER)
+        if not os.path.exists(self.FULL_IN_LOG_IMG_FOLDER):
+            os.mkdir(self.FULL_IN_LOG_IMG_FOLDER)
+        # Input image to OP
+        self.FULL_IN_TMP_IMG_FOLDER = self.DATA_FOLDER + self.INPUT_SNAP_IMAGE_FOLDER
+        glb.logger.debug(self.FULL_IN_TMP_IMG_FOLDER)
+        if not os.path.exists(self.FULL_IN_TMP_IMG_FOLDER):
+            os.mkdir(self.FULL_IN_TMP_IMG_FOLDER)
+        self.SNAPSHOT_TMP_IMAGE_NAME = self.FULL_IN_TMP_IMG_FOLDER + "/" + self.SNAPSHOT_TMP_NAME + ".jpg"
+        # self.INPUT_IMAGES_FOLDER_NAME = "Src-Images"
+        # OP results
+        if not os.path.exists(self.OUTPUT_FOLDER_NAME):
+            os.mkdir(self.OUTPUT_FOLDER_NAME)
+        self.JSON_FOLDER_NAME = self.OUTPUT_FOLDER_NAME + "/" + "Jsons"
+        self.SKELETON_IMAGE_FOLDER_NAME = self.OUTPUT_FOLDER_NAME + "/" + "Images"
+        if not os.path.exists(self.JSON_FOLDER_NAME):
+            os.mkdir(self.JSON_FOLDER_NAME)
+        if not os.path.exists(self.SKELETON_IMAGE_FOLDER_NAME):
+            os.mkdir(self.SKELETON_IMAGE_FOLDER_NAME)
+        self.JSON_TMP_FILE = self.JSON_FOLDER_NAME + "/" + self.SNAPSHOT_TMP_NAME + "_keypoints.json"
+        self.SEKELETON_TMP_IMG = self.SKELETON_IMAGE_FOLDER_NAME + "/" + self.SNAPSHOT_TMP_NAME + "_rendered.png"
+
+        self.OpInput = " --image_dir " + self.FULL_IN_TMP_IMG_FOLDER
+        self.OpJsonResult = " --write_json " + self.JSON_FOLDER_NAME
+        self.OpSkelatonImageResult = " --write_images " + self.SKELETON_IMAGE_FOLDER_NAME
+
+    def snap_frame_2_image(self):
+        webcam = cv2.VideoCapture(OpenPoseClass.CAMERA_ID)
+        check, frame = webcam.read()
+        cv2.imwrite(filename=self.SNAPSHOT_TMP_IMAGE_NAME, img=frame)
+        webcam.release()
+        glb.logger.debug(
+            f'A snapshot was taken from the WEB camera {self.CAMERA_ID} and saved as a jpg image to file {self.SNAPSHOT_TMP_IMAGE_NAME}')
+
+    # Snap selected specific video frame to image file
+    def Snap_video_frame_to_image_file(self, camid, imagefile):
+        cap = cv2.VideoCapture(camid)
+        # Check if the webcam is opened correctly
+        if not cap.isOpened():
+            raise IOError("Cannot open webcam")
+        while True:
+            ret, frame = cap.read()
+            frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
+            cv2.imshow('Input', frame)
+            c = cv2.waitKey(1)
+            if c == 27:
+                cv2.imwrite(filename=imagefile, img=frame)
+                break
+        cap.release()
+        cv2.destroyAllWindows()
+
+    def Run(self):
+        os.system(
+            "cd " + self.ROOT_DIR + " & " + self.OP_PROGRAM_NAME + self.OpInput + self.OpJsonResult + self.OpSkelatonImageResult)
+        glb.logger.debug(
+            "cd " + self.ROOT_DIR + " & " + self.OP_PROGRAM_NAME + self.OpInput + self.OpJsonResult + self.OpSkelatonImageResult)
+
+    def print_all_defenitions(self):
+        print(f'self.SNAPSHOT_TMP_NAME : {self.SNAPSHOT_TMP_NAME}')
+        print(f'self.ROOT_DIR : {self.ROOT_DIR}')
+        print(f'self.OP_PROGRAM_NAME : {self.OP_PROGRAM_NAME}')
+        print(f'self.DATA_FOLDER : {self.DATA_FOLDER}')
+        print(f'self.INPUT_SNAP_IMAGE_FOLDER : {self.INPUT_SNAP_IMAGE_FOLDER}')
+        print(f'self.OUTPUT_FOLDER_NAME : {self.OUTPUT_FOLDER_NAME}')
+        print(f'self.CAMERA_ID : {self.CAMERA_ID}')
+
+        print(f'self.FULL_IN_LOG_IMG_FOLDER : {self.FULL_IN_LOG_IMG_FOLDER}')
+
+        print(f'self.FULL_IN_TMP_IMG_FOLDER : {self.FULL_IN_TMP_IMG_FOLDER}')
+        print(f'self.SNAPSHOT_TMP_IMAGE_NAME : {self.SNAPSHOT_TMP_IMAGE_NAME}')
+
+        # OP results
+        print(f'self.JSON_FOLDER_NAME : {self.JSON_FOLDER_NAME}')
+        print(f'self.SKELETON_IMAGE_FOLDER_NAME : {self.SKELETON_IMAGE_FOLDER_NAME}')
+        # print(f'self.FULL_JSON_TMP_PATH : {self.FULL_JSON_TMP_PATH}')
+        # print(f'self.FULL_IMAGE_TMP_PATH : {self.FULL_IMAGE_TMP_PATH}')
+        print(f'self.JSON_TMP_FILE : {self.JSON_TMP_FILE}')
+        print(f'self.SEKELETON_TMP_IMG : {self.SEKELETON_TMP_IMG}')
+
+        print(f'self.OpInput : {self.OpInput}')
+        print(f'self.OpJsonResult : {self.OpJsonResult}')
+        print(f'self.OpSkelatonImageResult : {self.OpSkelatonImageResult}')
+
+    def op_json_to_df(self,json_file):
+        selected_person = 0
+        no_of_vertexes = 25
+        vertex_dimension = 3
+        body_part_name = 'pose_keypoints_2d'
+
+        with open(json_file) as jsonFile:
+            jsonObject = json.load(jsonFile)
+            jsonFile.close()
+
+        version = jsonObject['version']
+        people = jsonObject['people']
+
+        body_vertexes_np = np.array(people[selected_person].get(body_part_name)).reshape(no_of_vertexes, vertex_dimension)
+
+        glb.logger.debug(version)
+        glb.logger.debug(body_vertexes_np.shape)
+
+        return pd.DataFrame(body_vertexes_np, columns=['X', 'Y', 'C'])
+
+    # Calculates all limb distances between vertices
+    def generate_ditance_np_matrix(self,body_df):
+        distances = pdist(body_df[['X', 'Y']].values, metric='euclidean')
+        return squareform(distances)
+
+    # Calculates limb distances between two specics vertices
+    def dist_between_2_vertexes(self,vertexes_df, v1, v2):
+        return (
+        self.all_vertexes_dist_matrix_np[v1][v2])  # Keep in mind vertexes id here are -1 compare to openpose vertexes ids
+
+
---
+++ main.py
@@ -0,0 +1,117 @@
+# Import required Libraries
+from tkinter import *
+from PIL import Image, ImageTk
+import cv2
+import datetime
+import os
+import glob
+import sys
+import logging
+import time
+import shutil
+
+from OpenPoseManager import OpenPoseClass
+import GlobalVars as glb
+
+global all_vertexes_dist_matrix_np
+
+
+# logger = logging.getLogger(__name__)
+glb.logger = logging.getLogger('LibertyLogger')
+glb.logger.setLevel(logging.DEBUG)
+# create console handler and set level to debug
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+# create formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# add formatter to ch
+ch.setFormatter(formatter)
+# add ch to logger
+glb.logger.addHandler(ch)
+
+
+# log to file
+# file_handler = logging.FileHandler('logfile.log')
+# formatter    = logging.Formatter('%(asctime)s : %(levelname)s : %(name)s : %(message)s')
+# file_handler.setFormatter(formatter)
+
+# add file handler to logger
+# logger.addHandler(file_handler)
+# # Logs
+# logger.debug('A debug message')
+# logger.info('An info message')
+# logger.warning('Something is not right.')
+# logger.error('A Major error has happened.')
+# logger.critical('Fatal error. Cannot continue')
+# logging.basicConfig(level=logging.DEBUG,
+#                         format='(%(threadName)-9s) %(message)s',)
+def calculateSkeleton(with_copy=True):
+    global all_vertexes_dist_matrix_np
+    if with_copy:
+        last_snaped_image = newest_file_in_folder(OpenPose.FULL_IN_LOG_IMG_FOLDER)
+        shutil.copy(last_snaped_image, OpenPose.SNAPSHOT_TMP_IMAGE_NAME)
+        glb.logger.debug(f'performed: shutil.copy({last_snaped_image}, {OpenPose.SNAPSHOT_TMP_IMAGE_NAME})')
+    OpenPose.Run()
+    body_vertexes_df = OpenPose.op_json_to_df(OpenPose.JSON_TMP_FILE)
+    all_vertexes_dist_matrix_np = OpenPose.generate_ditance_np_matrix(body_vertexes_df)
+
+def takeSnapshot(op):
+    # grab the current timestamp and use it to construct the
+    # output path
+    global cv2image
+    glb.logger.debug("Try to take snapshot")
+    ts = datetime.datetime.now()
+    filename = "{}.jpg".format(ts.strftime("%Y-%m-%d_%H-%M-%S"))
+    p = os.path.sep.join((outputPath, filename))
+    # save the file
+    cv2.imwrite(p, cv2image.copy())
+    glb.logger.debug("[INFO] saved {}".format(p))
+    calculateSkeleton(True)
+
+# Define function to show frame
+def show_frames():
+    global cv2image
+    # Get the latest frame and convert into Image
+    cv2image = cv2.cvtColor(cap.read()[1], cv2.COLOR_BGR2RGB)
+    img = Image.fromarray(cv2image)
+    # Convert image to PhotoImage
+    imgtk = ImageTk.PhotoImage(image=img)
+    label.imgtk = imgtk
+    label.configure(image=imgtk)
+    # Repeat after an interval to capture continiously
+    label.after(20, show_frames)
+
+#Find the newest file in  the folder
+def newest_file_in_folder(path):
+    files = os.listdir(path)
+    paths = [os.path.join(path, basename) for basename in files]
+    return max(paths, key=os.path.getctime)
+
+
+OpenPose=OpenPoseClass('C:/24D/LibertySnapshot')
+outputPath = OpenPose.FULL_IN_LOG_IMG_FOLDER
+
+# Create an instance of TKinter Window or frame
+win = Tk()
+# Set the size of the window
+win.geometry("645x545")
+# create a button, that when pressed, will take the current
+# frame and save it to file
+# btn = Button(win, text="Snapshot",command=takeSnapshot)
+btn = Button(win, text="Snapshot!",
+             command=lambda myOp=OpenPose : takeSnapshot(myOp))
+             #command=takeSnapshot)
+btn.grid(row=1, column=0)
+
+btn_prev = Button(win, text="Measure Previous Frame",
+             command=lambda myOp=False : calculateSkeleton(myOp))
+             #command=takeSnapshot)
+btn_prev.grid(row=2, column=0)
+
+# Create a Label to capture the Video frames
+label = Label(win)
+label.grid(row=0, column=0)
+cap = cv2.VideoCapture(0)
+
+show_frames()
+win.mainloop()
